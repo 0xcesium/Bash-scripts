430c211efb9d82865517f94184facbe81af32f88
Cesium <ilal.jim@gmail.com>
2017-06-01 14:31:34 +0200

    Create etc-hosts-IP-exchange.sh

---
+++ etc-hosts-IP-exchange.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# UPDATE THE IP OF A MACHINE IN ETC/HOSTS
+#
+# eg : ./hmodif.sh MACHINE-NAME 10.10.10.10
+#
+# S'assurer de lancer le script avec sudo.
+# Be sure to run it within root privileges.
+#
+# Version de production : v4 - 23/11/2016
+# Jimmy B.
+# @133_cesium
+
+sed -i "/"$1"/c\\"$2'\t\t'$1 /etc/hosts
+printf "Changement apporté à /etc/hosts\n$2     $1\n"
